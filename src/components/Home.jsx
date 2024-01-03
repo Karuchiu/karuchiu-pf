f4286af6afedcf9714cdfa8d7d3e3f4696d5e037
Karuchiu <karuchiu8@gmail.com>
2024-01-03 13:55:44 +0300

    Updated Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../assets/heroImage.png";
+import KDonImage from "../assets/KDon.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
@@ -11,13 +11,14 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+          <h2 className="text-3xl sm:text-7xl font-bold text-white">
+            I'm Karuchiu Wamahiga
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            I'm a Nairobi-based Android Developer.
+            I have 2 years of experience building and designing software.
+            Currently, I love to work using
+            Jetpack Compose. I'm currently learning Springboot.
           </p>
 
           <div>
@@ -37,7 +38,7 @@ const Home = () => {
 
         <div>
           <img
-            src={HeroImage}
+            src={KDonImage}
             alt="my profile"
             className="rounded-2xl mx-auto w-2/3 md:w-full"
           />
